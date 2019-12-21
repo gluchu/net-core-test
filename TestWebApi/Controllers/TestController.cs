44a76c56e7349fd910cc179a51e54408f1644403
Michał <gluchu@wp.pl>
2019-12-21 12:56:27 +0000

    TestWebApi

---
+++ TestWebApi/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestWebApi.Controllers
+{
+    [ApiController]
+    [Route("Test")]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+           return Ok("asdss");
+        }
+    }
+}
